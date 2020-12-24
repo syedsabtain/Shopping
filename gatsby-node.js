7f7f0610be1a3df492a83229141c0f776f7469c1
Syed <sabtain.info@gmail.com>
2020-12-24 22:41:31 +0500

    programatically creating pages for Shopify

---
+++ gatsby-node.js
@@ -0,0 +1,42 @@
+const path = require('path')
+exports.createPages = async({graphql,actions})=>{
+
+    const{createPage} = actions
+    const result = await graphql(`
+    query {
+      allShopifyProduct(sort: { fields: [title] }) {
+        edges {
+          node {
+            title
+            images {
+              originalSrc
+            }
+            shopifyId
+            handle
+            description
+            availableForSale
+            priceRange {
+              maxVariantPrice {
+                amount
+              }
+              minVariantPrice {
+                amount
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  result.data.allShopifyProduct.edges.forEach(({node})=>{
+      createPage({
+          path:`/Shopify/${node.handle}`,
+          component:path.resolve(`./src/templates/Products.tsx`),
+          context:{
+              product:node,
+          }
+      })
+  })
+
+}
