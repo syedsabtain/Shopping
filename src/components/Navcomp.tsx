1bccb36a84054dfe7d791e66dfa8685fc724a0a4
Syed <sabtain.info@gmail.com>
2020-12-31 02:55:20 +0500

    ADD Home in Nav and many more

--- src/components/Navcomp.tsx
+++ src/components/Navcomp.tsx
@@ -1,24 +1,30 @@
 import React from 'react'
 import {Link} from 'gatsby'
+import {useLocation} from '@reach/router'
 
 const Navcomp =()=>{
 
+  const location = useLocation();
+  
     return(
         <ul className="nav d-flex justify-content-center">
-  <li className="nav-item">
-    <Link className="nav-link active" aria-current="page" to='/Stripe'>Stripe</Link>
+           <li className="nav-item">
+    <Link className={location.pathname === '/' ? "nav-link navdactive " : "nav-link "} aria-current="page" to='/'>HOME</Link>
   </li>
   <li className="nav-item">
-    <Link className="nav-link" to='/Snipcart'>SnipCart</Link>
+    <Link className={location.pathname === '/Stripe' ? "nav-link navdactive " : "nav-link "} aria-current="page" to='/Stripe'>Stripe</Link>
   </li>
   <li className="nav-item">
-    <Link className="nav-link" to='/OpenSource'>OpenSource</Link>
+    <Link className={location.pathname === '/Snipcart' ? "nav-link navdactive" :"nav-link "} to='/Snipcart'>SnipCart</Link>
   </li>
   <li className="nav-item">
-    <Link className="nav-link" to='/Shopify'>Shopify</Link>
+    <Link className={location.pathname === '/OpenSource' ? "nav-link navdactive" : "nav-link"} to='/OpenSource'>OpenSource</Link>
   </li>
   <li className="nav-item">
-    <Link className="nav-link" to='/ShopifyGraphlql'>Shopify Using Graphql</Link>
+    <Link className={location.pathname === '/Shopify' ? "nav-link navdactive" :"nav-link"} to='/Shopify'>Shopify</Link>
+  </li>
+  <li className="nav-item">
+    <Link className={location.pathname === '/ShopifyGraphlql' ? "nav-link navdactive": "nav-link"} to='/ShopifyGraphlql'>Shopify Using Graphql</Link>
   </li>
 
 </ul>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ export default()=>{
       <div className='container mt-5 '>
         
         <h1 className='mt-5 text-center'>Welcome to Stripe And Snipcart....</h1>
+        <h1 className='mt-2 text-center'>It's For Testing Purpose</h1>
     </div>
     </Maintemp>
   )
